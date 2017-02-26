b4a1dde3f07feab218309de1321e617017666685
Gary Swift <garyswiftmail@gmail.com>
2017-02-26 19:29:54 +0000

    Removed submenu

--- brightlight-menu.php
+++ brightlight-menu.php
@@ -18,22 +18,45 @@ class WP_Swift_Brightlight_Admin_menu {
     }
 
 	public function brightlight_admin_menu() {
-		// Create top-level menu item
+		$show_in_menu=false;
+		$slug = 'wp-swift-brightlight-main-menu';
+		if ($show_in_menu) {
+			$function = array($this, 'brightlight_complex_main');
+		}
+		else {
+			$function = '';
+		}
+		# Create top-level menu item
 		add_menu_page( 'BrightLight Configuration Page',
-		   'BrightLight', 'manage_options',
-		   'wp-swift-brightlight-main-menu', array($this, 'brightlight_complex_main'),
+		   'BrightLight', 
+		   'manage_options',
+		   'wp-swift-brightlight-main-menu', 
+		   '',//array($this, 'brightlight_complex_main'),
 		   plugins_url( 'favicon-16x16.png', __FILE__ ) );
-		// Create a sub-menu under the top-level menu
-		add_submenu_page( 'wp-swift-brightlight-main-menu',
-		   'BrightLight Menu Sub-Config Page', 'Sub-Config Page',
-		   'manage_options', 'brightlight-sub-menu',
-		   array($this, 'brightlight_my_complex_submenu') );
+		# Create a sub-menu under the top-level menu
+		// add_submenu_page( $slug,
+		//    'BrightLight Menu Sub-Config Page', 
+		//    'Sub-Config Page',
+		//    'manage_options', 
+		//    'brightlight-sub-menu',
+		//    array($this, 'brightlight_my_complex_submenu') );
+		// if ($show_in_menu===false) {}
+		remove_submenu_page('wp-swift-brightlight-main-menu', 'wp-swift-brightlight-main-menu');
 	}
 	public function brightlight_complex_main() {
 
 	}
 	public function brightlight_my_complex_submenu() {
-
+		$show_in_menu=false;
+		$slug = 'wp-swift-brightlight-main-menu';
+		if ($show_in_menu) {
+			$function = array($this, 'brightlight_complex_main');
+		}
+		else {
+			$function = '';
+		}
+		echo "<pre>"; var_dump($function); echo "</pre>";
+		echo "<pre>"; var_dump($array); echo "</pre>";
 	}	
 }
 $wp_swift_brightlight_admin_menu = new WP_Swift_Brightlight_Admin_menu();
